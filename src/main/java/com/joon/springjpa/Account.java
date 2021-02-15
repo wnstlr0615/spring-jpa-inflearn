068190125956e3c07473a3df0c72707a4301d0e6
wnstlr0615 <ryan0@kakao.com>
2021-02-15 09:55:32 +0900

    8.JPA 프로그래밍: Value 타입 맵핑

--- src/main/java/com/joon/springjpa/Account.java
+++ src/main/java/com/joon/springjpa/Account.java
@@ -24,4 +24,16 @@ public class Account {
     @Transient
     private String no;
 
+    @Embedded
+    @AttributeOverrides(
+           value = { @AttributeOverride(name = "street", column = @Column(name = "home_street")),
+                   @AttributeOverride(name="city", column = @Column(name = "home_city")),
+                   @AttributeOverride(name="state", column = @Column(name = "home_state")),
+                   @AttributeOverride(name="zipCode", column = @Column(name = "home_zip_code"))}
+    )
+    private Address  homeAddress;
+
+    @Embedded
+    private Address officeAddress;
+
 }
---
+++ src/main/java/com/joon/springjpa/Address.java
@@ -0,0 +1,15 @@
+package com.joon.springjpa;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String zipCode;
+
+}
