cfe021c53ea31960b4fe1677010953a4314c35f5
wnstlr0615 <ryan0@kakao.com>
2021-02-23 18:18:42 +0900

    29.	스프링 데이터 Common: Web 4부: HATEOAS

--- src/main/java/com/joon/springjpa/post/PostController.java
+++ src/main/java/com/joon/springjpa/post/PostController.java
@@ -1,8 +1,10 @@
 package com.joon.springjpa.post;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +26,10 @@ public class PostController {
     public String getPost1(@PathVariable("id") Post  post){
         return post.getTitle();
     }
+
     @GetMapping("/posts")
-    public Page<Post> getPosts(Pageable pageable){
-        return postRepository.findAll(pageable);
+    public PagedModel<EntityModel<Post>> getPosts(Pageable pageable, PagedResourcesAssembler<Post> assembler){
+        return assembler.toModel(postRepository.findAll(pageable));
     }
+
 }
